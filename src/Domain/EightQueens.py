6dd6403f74d5a9f98e2ae626b79a066a87cfb056
Richard Nunes <richardl1010@outlook.com>
2024-05-23 16:39:15 -0300

    REFACTOR: module renamed because of incompatibility)

---
+++ src/Domain/EightQueens.py
@@ -0,0 +1,11 @@
+from Domain.HeuristicProblem import HeuristicProblem, Solution
+
+# TODO: Implement the solution using an array
+
+
+class EightQueenSolution(Solution):
+    ...
+
+
+class EightQueenProblem(HeuristicProblem):
+    ...
