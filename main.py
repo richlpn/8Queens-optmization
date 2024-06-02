164255703199835f2055e6faf10717b3f1f68a6a
Richard Nunes <richardl1010@outlook.com>
2024-06-02 18:34:05 -0300

    ADD: Main Base

---
+++ main.py
@@ -0,0 +1,10 @@
+from src.Algorithm.HillClimbingRestart import HillClimbingRestart
+from src.Domain.EightQueens import EightQueenProblem
+
+
+def main():
+    problem = EightQueenProblem()
+
+
+if __name__ == '__main__':
+    main()
