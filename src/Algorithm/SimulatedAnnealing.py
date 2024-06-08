cd8d4e6126e0fc9221ee55d36ae8bca924bf15ed
Richard Nunes <richardl1010@outlook.com>
2024-06-08 19:37:04 -0300

    REFACTOR: cleaner metrics system

--- src/Algorithm/SimulatedAnnealing.py
+++ src/Algorithm/SimulatedAnnealing.py
@@ -10,12 +10,15 @@ class SA:
     _domain: HeuristicProblem
     _init_temp: float
     _cooling_rate: float
+    metrics: list[tuple[int, float]]
 
     def __init__(self, problem: HeuristicProblem,
                  initial_temperature: float, cooling_rate: float,) -> None:
         self._domain = problem
         self._init_temp = initial_temperature
         self._cooling_rate = cooling_rate
+        self.metrics = []
+        self._obj_calls = 0
 
     def acceptance_probability(self, current_fitness, new_fitness, temperature):
 
@@ -25,12 +28,10 @@ class SA:
             return math.exp((new_fitness - current_fitness) / temperature)
 
     def objective_function(self, solution: Solution) -> float:
+        self._obj_calls += 1
         return solution.evaluation
 
     def solve(self, size: int) -> Solution:
-
-        self.metrics = []
-        obj_calls = 1  # Objective is called once before the loop
 
         current_solution = self._domain.create_solution(size=size)
         current_fitness = self.objective_function(current_solution)
@@ -44,7 +45,6 @@ class SA:
 
             for neighbor in self._domain.generate_neighbor(current_solution):
                 new_fitness = self.objective_function(neighbor)
-                obj_calls += 1
 
                 acceptance_prob = self.acceptance_probability(
                     current_fitness, new_fitness, temperature)
@@ -59,6 +59,6 @@ class SA:
 
             temperature *= self._cooling_rate
 
-            self.metrics.append((obj_calls, best_fitness))
+            self.metrics.append((self._obj_calls, best_fitness))
 
         return best_solution
